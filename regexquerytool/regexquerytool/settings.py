9fd193108f45c19c900db118a43276f9653c3379
Yasmin Hillis <jasminehillis94@gmail.com>
2020-11-02 20:38:41 +0200

    update pipfile

--- regexquerytool/regexquerytool/settings.py
+++ regexquerytool/regexquerytool/settings.py
@@ -91,7 +91,7 @@ MIDDLEWARE = [
 
 sys.path.append(os.path.join(BASE_DIR, 'regexquerytool'))
 
-ROOT_URLCONF = 'regexquerytool.regexquerytool.urls'
+ROOT_URLCONF = 'regexquerytool.urls'
 
 TEMPLATES = [
     {
