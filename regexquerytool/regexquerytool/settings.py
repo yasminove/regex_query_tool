8ddfbb4dcc1a136a61c5ce430f3adb751a8ec4ae
Yasmin Hillis <jasminehillis94@gmail.com>
2020-11-02 17:39:21 +0200

    edit settings

--- regexquerytool/regexquerytool/settings.py
+++ regexquerytool/regexquerytool/settings.py
@@ -59,7 +59,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'users',
+    'regexquerytool.regexquerytool.users',
     'rest_framework', 
     'frontend',
     'regextool',
