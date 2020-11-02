df3b9d36e6bc3b54443c5e8ae1758caa0f345671
Yasmin Hillis <jasminehillis94@gmail.com>
2020-11-02 12:04:59 +0200

    edit wsgi

--- regexquerytool/regexquerytool/wsgi.py
+++ regexquerytool/regexquerytool/wsgi.py
@@ -16,6 +16,6 @@ if path not in sys.path:
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'regexquerytool.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yasmin.settings')
 
 application = get_wsgi_application()
