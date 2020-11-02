2f0391abd2df2c42b016f095acf005167e040600
Yasmin Hillis <jasminehillis94@gmail.com>
2020-11-02 17:31:03 +0200

    Procfile

--- regexquerytool/regexquerytool/wsgi.py
+++ regexquerytool/regexquerytool/wsgi.py
@@ -9,22 +9,22 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 import sys
-from pathlib import Path
-path = 'regexquerytool.settings'
+# from pathlib import Path
+# path = 'regexquerytool.settings'
 
 from django.core.wsgi import get_wsgi_application    
 
-if path not in sys.path:
-    sys.path.append(path)
+# if path not in sys.path:
+#     sys.path.append(path)
 
 
     
 
 
-project_home = u'/app/regexquerytool/regexquerytool/wsgi.py'
+# project_home = u'/app/regexquerytool/regexquerytool/wsgi.py'
 
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'regexquerytool.settings'    
+os.environ['DJANGO_SETTINGS_MODULE'] = '.settings'    
 
 
 
