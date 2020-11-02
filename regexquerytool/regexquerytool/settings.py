b12577c2656ac7d1b840067b62d18a83e88c44ab
Yasmin Hillis <jasminehillis94@gmail.com>
2020-11-02 18:43:30 +0200

    add env variables

--- regexquerytool/regexquerytool/settings.py
+++ regexquerytool/regexquerytool/settings.py
@@ -23,13 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+riz0y46l$)qoma@-*rkcj3sy9wr*g!$s%x-q04p_+=l@3l#&k'
+# SECRET_KEY = '+riz0y46l$)qoma@-*rkcj3sy9wr*g!$s%x-q04p_+=l@3l#&k'
+
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['regextool.herokuapp.com']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
